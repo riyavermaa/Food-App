70b4f06ffb9195fe81a79848cf45d12a3cef923c
Riya Verma <riyaverma0529@gmail.com>
2024-08-05 16:36:30 +0530

    uploaded files

---
+++ src/components/Layouts/Header.jsx
@@ -0,0 +1,52 @@
+import { useState } from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom';
+import Logo from '../../assets/foodlogo.jpg';
+import "../../styles/HeaderStyle.css";
+
+
+const Header = () => {
+    const [nav, setNav] = useState(false);
+
+    // Scroll Navbar
+    const changeValueOnScroll = () => {
+        const scrollValue = document?.documentElement?.scrollTop;
+        scrollValue > 100 ? setNav(true) : setNav(false);
+    };
+
+    window.addEventListener("scroll", changeValueOnScroll);
+    return (
+        <header>
+            <Navbar collapseOnSelect expand="lg" className={`${nav === true ? "sticky" : ""}`}>
+                <Container>
+                    <Navbar.Brand>
+                        <Link to="/" className='logo'>
+                            <img src={Logo} alt="Logo" className='img-fluid' />
+                        </Link>
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="ms-auto">
+                            <Nav.Link as={Link} to="/">Home</Nav.Link>
+                            <Nav.Link as={Link} to="/about">About</Nav.Link>
+                            <Nav.Link as={Link} to="/menu">Our Menu</Nav.Link>
+                            <Nav.Link as={Link} to="/shop">Shop</Nav.Link>
+                            <Nav.Link as={Link} to="/blog">Blog</Nav.Link>
+                            <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
+                            <Nav.Link as={Link} to="/">
+                                <div className="cart">
+                                    <i className="bi bi-bag-plus-fill fs-5"></i>
+                                    <em className='roundpoint'>2</em>
+                                </div>
+                            </Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </header>
+    );
+}
+
+export default Header;
